0c73c7147da45821526c2565a20f33b5bf1cccb7
turo11192 <124545311+turo11192@users.noreply.github.com>
2023-03-26 19:44:50 -0600

    Add files via upload

---
+++ LeerArchivos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeraAplicacion
+{
+    internal class LeerArchivos
+    {
+        public void lecturaDeArchivos()
+        {
+            System.IO.StreamReader archivo = null;
+
+            try
+            {
+                string linea;
+                int contador = 0;
+                string path = @"C:\\Users\\artur\\OneDrive\\Escritorio\\CoC.txt"; //write here te file path
+
+                archivo = new System.IO.StreamReader(path);
+
+                while ((linea = archivo.ReadLine()) != null)
+                {
+                    Console.WriteLine(linea);
+                    contador++;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error" + e.Message);
+            }
+        }
+
+    }
+}
